43fbf636008109fada5ccea9865d9864467ead0b
Kamaufaith <161581340+Kamaufaith@users.noreply.github.com>
2024-06-24 11:36:17 +0300

    Add files via upload

---
+++ text_interface/target/debug/deps/text_interface-46ef9b8eb5e61991.d
@@ -0,0 +1,5 @@
+/home/hp/faith/text_interface/target/debug/deps/text_interface-46ef9b8eb5e61991: src/main.rs
+
+/home/hp/faith/text_interface/target/debug/deps/text_interface-46ef9b8eb5e61991.d: src/main.rs
+
+src/main.rs:
---
+++ text_interface/target/debug/text_interface.d
@@ -0,0 +1,1 @@
+/home/hp/faith/text_interface/target/debug/text_interface: /home/hp/faith/text_interface/src/main.rs
